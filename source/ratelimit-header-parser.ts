8c7390e344a2bffab83792ef17f83491068bc1e5
Nathan Friedly <nathan@nfriedly.com>
2023-08-20 23:48:33 -0400

    parse separate headers and various reset formats

--- source/ratelimit-header-parser.ts
+++ source/ratelimit-header-parser.ts
@@ -8,31 +8,72 @@ export type RateLimit = {
     // todo: policy
 }
 
-type ResponseOrHeadersObject = 
-    /* Node.js things */
-    ServerResponse | IncomingHttpHeaders | OutgoingHttpHeaders |
-    /* fetch things */
-    Response | Headers;
+export type RateLimitOptions = {
+    reset?: 'date' | 'unix' | 'seconds' | 'milliseconds',
+}
 
-export function parseRateLimitHeaders(input: ResponseOrHeadersObject): RateLimit | undefined {
+// node or fetch
+type ResponseObject = ServerResponse | Response; 
+type HeadersObject =  IncomingHttpHeaders | OutgoingHttpHeaders | Headers | Object;
+
+export function parseRateLimit(input: ResponseObject | HeadersObject, options?: RateLimitOptions): RateLimit | undefined {
     if ('headers' in input && typeof input.headers === 'object' && !Array.isArray(input.headers)) {
-        return parseRateLimitHeaders(input.headers)
+        return parseRateLimit(input.headers, options)
     } else if ('getHeaders' in input && typeof input.getHeaders === 'function') {
-        return parseRateLimitHeaders(input.getHeaders())
+        return parseRateLimit(input.getHeaders(), options)
+    } else {
+        return parseHeadersObject(input, options)
     }
-    if (input['ratelimit']) {
-        return parseCombinedRateLimitHeader(input['ratelimit'])
+}
+
+function parseHeadersObject(input: HeadersObject, options: RateLimitOptions | undefined): RateLimit | undefined {
+    let combined = getHeader(input, 'ratelimit')
+    if (combined ) return parseCombinedRateLimitHeader(combined);
+    
+    let prefix;
+    if (getHeader(input, 'ratelimit-remaining')) {
+        prefix = 'ratelimit-'
+    } else if (getHeader(input, 'x-ratelimit-remaining')) {
+        prefix = 'x-ratelimit-'
+    } else {
+        // todo: handle vendor-specific headers here - see https://github.com/ietf-wg-httpapi/ratelimit-headers/issues/25
+        return;
     }
-    // todo: handle individual headers
+
+    const limit = num(getHeader(input, `${prefix}limit`))
+    const remaining = num(getHeader(input, `${prefix}remaining`))
+
+    let reset: Date|undefined = undefined;
+    const resetRaw = getHeader(input, `${prefix}reset`)
+    const resetType = options?.reset;
+    if (resetType == 'date') reset = parseResetDate(resetRaw ?? '');
+    else if (resetType == 'unix') reset = parseResetUnix(resetRaw ?? '');
+    else if (resetType == 'seconds') reset = parseResetSeconds(resetRaw ?? '');
+    else if (resetType == 'milliseconds') reset = parseResetMilliseconds(resetRaw ?? '');
+    else if (resetRaw) reset = parseResetAuto(resetRaw)
+    else {
+        // fallback to retry-after
+        const retryAfter = getHeader(input, 'retry-after');
+        if (retryAfter) {
+            reset = parseResetUnix(retryAfter)
+        }
+    }
+
+    return {
+        limit,
+        current: limit - remaining,
+        remaining,
+        reset
+    }
 }
 
 const reLimit = /limit\s*=\s*(\d+)/i
 const reRemaining = /remaining\s*=\s*(\d+)/i
 const reReset = /reset\s*=\s*(\d+)/i
 export function parseCombinedRateLimitHeader(input: string): RateLimit {
-    const limit = parseInt(reLimit.exec(input)?.[1] ?? '-1', 10);
-    const remaining = parseInt(reRemaining.exec(input)?.[1] ?? '-1', 10);
-    const resetSeconds = reReset.exec(input)?.[1] ?? '-1';
+    const limit = num(reLimit.exec(input)?.[1]);
+    const remaining = num(reRemaining.exec(input)?.[1]);
+    const resetSeconds = num(reReset.exec(input)?.[1]);
     const reset = secondsToDate(resetSeconds);
     return {
         limit,
@@ -42,8 +83,55 @@ export function parseCombinedRateLimitHeader(input: string): RateLimit {
     }
 }
 
-function secondsToDate(seconds: string | number): Date {
+function secondsToDate(seconds: number): Date {
     const d = new Date();
-    d.setSeconds(d.getSeconds() + (+seconds));
+    d.setSeconds(d.getSeconds() + seconds);
     return d;
+}
+
+function num(input: string | number | undefined): number {
+    if (typeof input == 'number') return input;
+    return parseInt(input ?? '', 10);
+}
+
+function getHeader(headers: HeadersObject, name: string): string | undefined {
+    if ('get' in headers && typeof headers.get === 'function') {
+        return headers.get(name) ?? undefined // returns null if missing, but everything else is undefined for missing values
+    }
+    return headers[name]
+}
+
+function parseResetDate(resetRaw: string): Date {
+    // todo: take the server's date into account, calculate an offset, then apply that to the current date
+    return new Date(resetRaw);
+}
+
+function parseResetUnix(resetRaw: string | number): Date {
+    let resetNum = num(resetRaw);
+    return new Date(resetNum * 1000);
+}
+
+function parseResetSeconds(resetRaw: string | number): Date {
+    let resetNum = num(resetRaw);
+    return secondsToDate(resetNum);
+}
+
+function parseResetMilliseconds(resetRaw: string | number): Date {
+    let resetNum = num(resetRaw);
+    return secondsToDate(resetNum/1000);
+}
+
+const reLetters = /[a-z]/i;
+function parseResetAuto(resetRaw: string): Date {
+    // if it has any letters, assume it's a date string
+    if (resetRaw.match(reLetters)) {
+        return parseResetDate(resetRaw)
+    }
+    const resetNum = num(resetRaw);
+    // looks like a unix timestamp
+    if (resetNum && resetNum > 1000000000) { // sometime in 2001
+        return parseResetUnix(resetNum)
+    }
+    // could be seconds or milliseconds (or something else!), defaulting to seconds
+    return parseResetSeconds(resetNum);
 }
--- test/parser-test.ts
+++ test/parser-test.ts
@@ -1,5 +1,26 @@
 import { describe, it, expect } from '@jest/globals'
-import { parseCombinedRateLimitHeader } from '../source/ratelimit-header-parser'
+import { parseCombinedRateLimitHeader, parseRateLimit } from '../source/ratelimit-header-parser'
+
+describe('parseRateLimitHeaders', () => {
+    it ('should handle X-RateLimit-* headers in a fetch Headers object', () => {
+        const headers = new Headers({
+            'X-RateLimit-Limit': '100',
+            'X-RateLimit-Remaining': '70',
+            'X-RateLimit-Reset': Math.floor(Date.now() / 1000).toString()
+        })
+        expect(parseRateLimit(headers)).toMatchObject({
+            limit: 100,
+            remaining: 70,
+            current: 30,
+            reset: expect.any(Date) // todo: mock the clock, then match to a specific date
+        })
+    })
+
+    // todo: test with other object types
+    // todo: test with various options
+})
+
+// todo: test parseResetAuto with various formats
 
 describe('parseCombinedRateLimitHeader', () => {
     it('should parse a combined header', () => {
