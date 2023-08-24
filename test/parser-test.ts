85f177e38d35494ccaaa97e99451046b1ed8f108
Nathan Friedly <nathan@nfriedly.com>
2023-08-24 17:22:33 -0400

    disable fetch Headers test on older node.js, add node-style headers test

--- test/parser-test.ts
+++ test/parser-test.ts
@@ -4,17 +4,37 @@ import {
 	parseRateLimit,
 } from '../source/ratelimit-header-parser.js'
 
+const itif = (condition: boolean) => (condition ? it : it.skip)
+
 describe('parseRateLimitHeaders', () => {
-	it('should handle X-RateLimit-* headers in a fetch Headers object', () => {
-		const headers = new Headers({
-			'X-RateLimit-Limit': '100',
-			'X-RateLimit-Remaining': '70',
-			'X-RateLimit-Reset': Math.floor(Date.now() / 1000).toString(),
-		})
+	// Note: Headers doesn't exist in node 16 or older
+	itif(typeof Headers !== 'undefined')(
+		'should handle X-RateLimit-* headers in a fetch Headers object',
+		() => {
+			const headers = new Headers({
+				'X-RateLimit-Limit': '100',
+				'X-RateLimit-Remaining': '70',
+				'X-RateLimit-Reset': Math.floor(Date.now() / 1000).toString(),
+			})
+			expect(parseRateLimit(headers)).toMatchObject({
+				limit: 100,
+				remaining: 70,
+				used: 30,
+				reset: expect.any(Date), // Todo: mock the clock, then match to a specific date
+			})
+		},
+	)
+
+	it('should handle RateLimit-* headers in a node-style headers object', () => {
+		const headers = {
+			'ratelimit-limit': '60',
+			'ratelimit-remaining': '20',
+			'ratelimit-reset': new Date().toISOString(),
+		}
 		expect(parseRateLimit(headers)).toMatchObject({
-			limit: 100,
-			remaining: 70,
-			used: 30,
+			limit: 60,
+			remaining: 20,
+			used: 40,
 			reset: expect.any(Date), // Todo: mock the clock, then match to a specific date
 		})
 	})
