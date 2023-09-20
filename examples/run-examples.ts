915b5c9ac3af4e763a202d6e72491e315a8040a5
Vedant K <gamemaker0042@gmail.com>
2023-09-20 15:39:14 +0530

    test: add `deno/fetch` example

--- examples/run-examples.ts
+++ examples/run-examples.ts
@@ -10,28 +10,39 @@ import task from 'tasuku'
 console.log(chalk.bold.inverse.green(' examples '))
 console.log()
 
-// Collect all the examples.
-const files = await globby('node/*.ts', 'deno/*.ts', 'browser/*.ts')
-const tasks = []
-for (const file of files) {
-	tasks.push(
-		task(file, async ({ setStatus, setOutput, setError }) => {
-			try {
-				// Run the example file with `tsx`.
-				const { stdout } = await $`npx tsx ${file}`
+const execute = async (files: string[], args: string[]): Promise<boolean> => {
+	const tasks = []
+	for (const file of files) {
+		tasks.push(
+			task(file, async ({ setStatus, setOutput, setError }) => {
+				try {
+					// Run the example file.
+					const { stdout } = await $`${args} ${file}`
 
-				setStatus('passed')
-				setOutput(stdout.replace(/\n/, '\n  ')) // Align the output correctly.
-			} catch (error) {
-				// Display the error.
-				setStatus('failed')
-				setError(error)
-			}
-		}),
-	)
+					setStatus('passed')
+					setOutput(stdout.replace(/\n/, '\n  ')) // Align the output correctly.
+				} catch (error) {
+					// Display the error.
+					setStatus('failed')
+					setError(error)
+				}
+			}),
+		)
+	}
+
+	// Run all the examples, concurrently.
+	const results = await Promise.all(tasks)
+
+	// If any example hasn't run succesfully, fail the test.
+	if (results.find((result) => result.state !== 'success')) return false
+	else return true
 }
 
-// Run all the examples, concurrently.
-const results = await Promise.all(tasks)
-// If any example hasn't run succesfully, fail the test.
-if (results.find((result) => result.state !== 'success')) process.exit(1)
+const node = await execute(await globby('node/*.ts'), ['npx', 'tsx'])
+const deno = await execute(await globby('deno/*.ts'), [
+	'deno',
+	'run',
+	'--allow-net',
+])
+
+if (!node || !deno) process.exit(1)
