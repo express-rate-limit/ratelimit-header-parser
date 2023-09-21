ce1cff4522145b7739df2a070cda5f5a7df65689
Vedant K <gamemaker0042@gmail.com>
2023-09-21 16:18:06 +0530

    test: use `getRateLimits` in `node/http-get` example

--- examples/node/http-get.ts
+++ examples/node/http-get.ts
@@ -2,13 +2,13 @@
 // Uses `http.get` to hit the Imgur API.
 
 import https from 'node:https'
-import { getRateLimit } from 'ratelimit-header-parser'
+import { getRateLimits } from 'ratelimit-header-parser'
 
 // Make a GET request to the Imgur API.
 https.get('https://api.imgur.com/post/v1/posts/t/aww', (response) => {
-	console.log('imgur ratelimit:', getRateLimit(response))
-
+	console.log('imgur ratelimit:', getRateLimits(response))
 	return response.resume()
 })
 
-// > imgur ratelimit:  { limit: 500, used: 1, remaining: 499, reset: 2023-09-19T14:19:30.297Z }
+// > imgur ratelimit: { limit: 500, used: 1, remaining: 499, reset: 2023-09-19T14:19:30.297Z }
+// >                  { limit: 12500, used: 1, remaining: 12499, reset: 2023-09-19T14:19:30.297Z }
